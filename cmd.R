966affd8156d7ffd52db5bc9e4db6b16971e92ae
Julien Barrere <julienbarrere3@gmail.com>
2022-04-11 10:30:59 +0200

    Bug fix

--- cmd.R
+++ cmd.R
@@ -1,5 +1,5 @@
 # Install targets package if needed
-if(!("targets" %in% installed.packages()) install.packages("targets")
+if(!("targets" %in% installed.packages())) install.packages("targets")
 # Load targets
 library(targets)
 # Launch the targets pipeline
