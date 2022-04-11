f192539edac8d4e22555d096607047289c0b23a9
Julien Barrere <julienbarrere3@gmail.com>
2022-04-11 10:25:10 +0200

    Added a batch script to launch targets pipeline

---
+++ batch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=FinnishNFI
+#SBATCH --account=project_2005142
+#SBATCH --time=00:01:00
+#SBATCH --mem-per-cpu=1000
+#SBATCH --partition=small
+
+module load r-env-singularity/4.0.5
+
+srun singularity_wrapper exec Rscript --no-save cmd.R
